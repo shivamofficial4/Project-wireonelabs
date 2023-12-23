9ce5323ffce81a0dc9c4910fe5a2fedb4c783bcf
shivamofficial4 <shivam.g@vraimatic.ai>
2023-12-23 13:09:15 +0530

    This-is-the-firdt-commit-of-the-day

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Router from './router/index';
+
+function App() {
+  return (
+    <div className="App">
+      <Router />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import store from './store/store';
+import { ThemeProvider } from 'styled-components';
+import theme from './theme/theme';
+import {BrowserRouter} from 'react-router-dom';
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
